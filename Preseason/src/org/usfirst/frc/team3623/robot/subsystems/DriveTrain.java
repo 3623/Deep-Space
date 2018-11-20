fe354361da8276edf76e3b7b6b1821aeb8be1e37
Eric Jenny <31317958+ericjenny3623@users.noreply.github.com>
2018-11-20 16:42:34 -0500

    Slight changes to drdivetrain class

--- Preseason/src/org/usfirst/frc/team3623/robot/subsystems/DriveTrain.java
+++ Preseason/src/org/usfirst/frc/team3623/robot/subsystems/DriveTrain.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3623.robot.subsystems;
 import java.io.IOException;
 
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 public class DriveTrain {
 	Spark frontLeft = new Spark(0);
@@ -10,14 +11,17 @@ public class DriveTrain {
 	Spark backLeft = new Spark(2);
 	Spark backRight = new Spark(3);
 	
+	SpeedControllerGroup leftMotors = new SpeedControllerGroup(frontLeft, backLeft);
+	SpeedControllerGroup  rightMotors = new SpeedControllerGroup(frontRight, backRight);
+	
 	
 	public void writeToLog() {
     };
 
-    public void outputToSmartDashboard() {
-	}
+    public void outputToSmartDashboard {	}
 
     public int stop() {
+    	stopMotor();
 		return 0;
 	}
 
