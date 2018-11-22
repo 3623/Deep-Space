cec47e9d28e81e5fc3f6fcea737e5c1566d5cf29
ClaireHigg <claire.higginson5@gmail.com>
2018-11-21 20:16:15 -0500

    I put in encoders kind of I put them in and made them reset when the robot starts but that's it for now I also added a place for the open Loop Control I think for the encoders too haven't done anything with it yet thoI put in encoders kind of I put them in and made them reset when the robot starts but that's it for now I also added a place for the open Loop Control I think for the encoders too haven't done anything with it yet tho

--- Preseason/src/org/usfirst/frc/team3623/robot/subsystems/DriveTrain.java
+++ Preseason/src/org/usfirst/frc/team3623/robot/subsystems/DriveTrain.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3623.robot.subsystems;
 
 import java.io.IOException;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
@@ -14,23 +15,30 @@ public class DriveTrain {
 	SpeedControllerGroup leftMotors = new SpeedControllerGroup(frontLeft, backLeft);
 	SpeedControllerGroup  rightMotors = new SpeedControllerGroup(frontRight, backRight);
 	
+	Encoder encLeft = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+	Encoder encRight = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+	
 	
 	public void writeToLog() {
     };
 
     public void outputToSmartDashboard {	}
 
-    public int stop() {
-    	stopMotor();
-		return 0;
+    public void stop() {
+    	leftMotors.disable();
+    	rightMotors.disable();
 	}
 
     public void zeroSensors() {
+    	encLeft.reset();
+    	encRight.reset();
+    	
 	}
+    
+   public void openLoopControl
 
 //    public void registerEnabledLoops(Looper enabledLooper) {
 //	}
- 
  
     
     public static void main(String[] args) throws IOException{}
