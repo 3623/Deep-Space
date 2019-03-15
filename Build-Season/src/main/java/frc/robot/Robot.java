4e318f9f6d8bf1252f141cf6109370102681d84a
Eric Jenny <31317958+ericjenny3623@users.noreply.github.com>
2019-03-15 15:13:44 -0400

    Max's Camera Code

--- Build-Season/src/main/java/frc/robot/Robot.java
+++ Build-Season/src/main/java/frc/robot/Robot.java
@@ -23,6 +23,12 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Grabber;
 import frc.robot.subsystems.Turret;
+
+import org.opencv.core.Point;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
 import edu.wpi.cscore.AxisCamera;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
@@ -66,6 +72,8 @@ public class Robot extends TimedRobot {
 
   Command autoCommand;
 
+  Thread m_visionThread;
+
 
   /**
    * This function is run when the robot is first started up and should be
@@ -93,9 +101,40 @@ public class Robot extends TimedRobot {
 
     driverControl = false;
 
-    AxisCamera maxCamera = CameraServer.getInstance().addAxisCamera("Max Cam","10.36.23.11");
+    m_visionThread = new Thread(() -> {
+      // Get the Axis camera from CameraServer
+      AxisCamera camera
+          = CameraServer.getInstance().addAxisCamera("axis-camera.local");
+      // Set the resolution
+      camera.setResolution(320, 240);
+      camera.setFPS(15);
 
-    drivetrain.zeroSensors();
+      // Get a CvSink. This will capture Mats from the camera
+      CvSink cvSink = CameraServer.getInstance().getVideo();
+      // Setup a CvSource. This will send images back to the Dashboard
+      CvSource outputStream
+          = CameraServer.getInstance().putVideo("Rectangle", 320, 240);
+
+      // Mats are very memory expensive. Lets reuse this Mat.
+      Mat mat = new Mat();
+
+      // This cannot be 'true'. The program will never exit if it is. This
+      // lets the robot stop this thread when restarting robot code or
+      // deploying.
+      while (!Thread.interrupted()) {
+        // Tell the CvSink to grab a frame from the camera and put it
+        // in the source mat.  If there is an error notify the output.
+        if (cvSink.grabFrame(mat) == 0) {
+          // Send the output the error.
+          outputStream.notifyError(cvSink.getError());
+          // skip the rest of the current iteration
+          continue;
+        }
+        outputStream.putFrame(mat);
+      }
+    });
+    m_visionThread.setDaemon(true);
+    m_visionThread.start();
   }
 
   /**
@@ -111,6 +150,7 @@ public class Robot extends TimedRobot {
 	  drivetrain.update(timer.getFPGATimestamp());
     elevator.updateStuff(); 
     turret.updateStuff();
+    
   }
 
   /**
