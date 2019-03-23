915f98b0ca22063d2439a19886eac949353f72cd
Eric Jenny <erc.hakawa@gmail.com>
2019-03-23 13:32:21 -0400

    Cleaned up

--- Build-Season/src/main/java/frc/util/Utils.java
+++ Build-Season/src/main/java/frc/util/Utils.java
@@ -3,7 +3,7 @@ package frc.util;
 public class Utils {
 
 	
-	public static Boolean threshold(double value, double goal, double epsilon) {
+	public static Boolean withinThreshold(double value, double goal, double epsilon) {
 		return Math.abs(value-goal) <= epsilon;
 	}
 
